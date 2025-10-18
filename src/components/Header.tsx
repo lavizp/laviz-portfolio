21bd5f3f70189231b404adcbb6e3ee2428e193d6
lavizp <pandeylaviz@gmail.com>
2025-10-18 16:08:38 +0000

    feat: tansstack init

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+
+  return (
+    <header className='flex mx-3 my-3'>
+      LavizP
+    </header>
+  )
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+ 
+
+  return (
+    <div className="">
+     asd
+
+    </div>
+  )
+}
